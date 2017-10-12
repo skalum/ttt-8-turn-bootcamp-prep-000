1d87349ad4fe41f68929167802e0b55bd1701666
Sam Kalum <stkalum@gmail.com>
2017-10-12 21:34:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,3 +9,11 @@ end
 def input_to_index(input)
   input.to_i - 1
 end
+
+def valid_move?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+  !!board[index] && board[index] != "" && board[index] != " "
+end
